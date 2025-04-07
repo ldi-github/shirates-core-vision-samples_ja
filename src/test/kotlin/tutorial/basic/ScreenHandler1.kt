8752757480eb74964148be24cc794b76ba0e9f97
ldi_nsenba <n.senba@ldi.co.jp>
2025-04-07 10:15:03 +0900

    8.1.7

--- src/test/kotlin/tutorial/basic/ScreenHandler1.kt
+++ src/test/kotlin/tutorial/basic/ScreenHandler1.kt
@@ -23,7 +23,8 @@ class ScreenHandler1 : VisionTest() {
                         it.tap("インターネット", last = true)
                     }.onScreen("[インターネット画面]") {
                         it.tap("AndroidWifi")
-                    }.tap("ネットワークとインターネット")
+                    }
+                    it.tap("ネットワークとインターネット")
                     /**
                      * onScreen("[ネットワークとインターネット画面]") が呼ばれます
                      */
@@ -32,6 +33,47 @@ class ScreenHandler1 : VisionTest() {
                      */
                 }.expectation {
                     it.exist("ネットワークの詳細")
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(20)
+    fun screenHandler_keep() {
+
+        scenario {
+            case(1) {
+                condition {
+                    it.macro("[Android設定トップ画面]")
+                }.action {
+                    onScreen("[ネットワークとインターネット画面]") { c ->
+                        it.tap("インターネット", last = true)
+                        c.keep = true   // スクリーンハンドラーは登録されたままになります。登録解除されません。
+                    }
+                    it.tap("ネットワークとインターネット")
+                }.expectation {
+                    it.exist("ネットワーク設定")
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(30)
+    fun screenHandler_permanent() {
+
+        scenario {
+            case(1) {
+                condition {
+                    it.macro("[Android設定トップ画面]")
+                }.action {
+                    onScreen("[ネットワークとインターネット画面]", permanent = true) {
+                        it.tap("インターネット", last = true)
+                    }
+                    it.tap("ネットワークとインターネット")
+                }.expectation {
+                    it.exist("ネットワーク設定")
                 }
             }
         }
