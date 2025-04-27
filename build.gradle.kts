04471666887553184da84b2637b891f771b45367
ldi_nsenba <n.senba@ldi.co.jp>
2025-04-28 00:50:54 +0900

    8.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-val shiratesCoreVersion = "8.2.0"
+val shiratesCoreVersion = "8.3.1"
 val appiumClientVersion = "9.4.0"
 
 val userHome = System.getProperty("user.home")
